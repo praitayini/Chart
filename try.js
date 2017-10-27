be8bd66d84cf234adb45f254fb36517c7af8361e
praitayini <praitayini@gmail.com>
2017-10-27 00:19:10 -0400

    yay

--- try.js
+++ try.js
@@ -1,7 +1,8 @@
 var response=jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json?$query=SELECT%20COUNT(*)')
     .then(  function(x) {
         console.log(x)
-        var count = parseInt(response.responseJSON[0]['count']);
+//debugger
+        var count = parseInt(x[0].count);
         var wk = document.getElementById('work')
         wk.innerHTML='';
         wk.innerHTML='Number of observartions = '+ count
