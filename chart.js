ba60e60fd07e22a92c1f077722885b5786bdee97
praitayini <praitayini@gmail.com>
2017-11-15 16:03:27 -0500

    h

--- chart.js
+++ chart.js
@@ -1,34 +1,76 @@
-var yourArray = [];
+jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
+  .then(function(dt){
+    var avg=function(a){
+      return a.reduce(function(a,b){
+        return a+b
+        })/a.length
+    }
+    console.log('data loaded: ',dt)
+    var x=dt.map(function(xi){
+      return parseFloat(xi.expected_rate_per_100_000_people)
+    })
+    avg(x)
+    // data in x, ready for stats
+    //var wk = document.getElementById('work')
+    //wk.innerHTML='Number of observartions = '+x.length+'<br>Average value : '+avg(x)
+    //wk.style.color='blue'
+    
+    //debugger
+})
 
-//jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
-  //.then(function(obj){
-    // var count = Object.keys(obj).length;
-         for (var i = 0; i < countPromises; i++) {
-         // alert(obj[i].patient_zipcode);
-         for (var j =0; j< countArray; j++)
-           yourArray.push(xx[i].responseJSON[j].year);
-        }
-        getDropDownList('year','year', $.unique(yourArray))
-        datas(xx,'');
-
-  
-
-
-
-function datas(obj,value){
+Plotly.d3.json('https://health.data.ny.gov/resource/5q8c-d6xq.json', function(items){
  var index;
  var pqi_name= [];
  var y1  = [];
  var y2  = [];
- for    (index = 0; index < obj.length; index++) {
-    pqi_name.push(obj[index].pqi_name);
-    y1.push(obj[index].observed_rate_per_100_000_people); //change
-    y2.push(obj[index].expected_rate_per_100_000_people);
+ var patient_zipcode= [];
+ var listofZipcode = [];
+    
+ for	(index = 0; index < items.length; index++) {
+ 	pqi_name.push(items[index].pqi_name);
+ 	y1.push(items[index].observed_rate_per_100_000_people);
+ 	y2.push(items[index].expected_rate_per_100_000_people);
+    patient_zipcode.push(items[index].patient_zipcode);
  }
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
+    }
+
+//var allCountryNames = unpack(rows, 'country'),
+  //  allYear = unpack(rows, 'year'),
+    //allGdp = unpack(rows, 'gdpPercap'),
+    
+    //currentCountry,
+    //currentGdp = [],
+    //currentYear = [];
+
+  for (index = 0; index < patient_zipcode.length; index++ ){
+    if (listofZipcode.indexOf(patient_zipcode[index]) === -1 ){
+      listofZipcode.push(patient_zipcode[index]);
+    }
+  }
+  
+  function getZipcode(chosenZipcode) {
+    var patient_zipcode = [];
+    for (index = 0 ; index < items.length ; index++){
+      if ( patient_zipcode[index] === chosenZipcode ) {
+        pqi_name.push(items[index].pqi_name);
+ 	   y1.push(items[index].observed_rate_per_100_000_people);
+ 	   y2.push(items[index].expected_rate_per_100_000_people);
+      } 
+    }
+  };
+
+// Default Country Data
+//setBubblePlot('Afghanistan');
+  
+function setBarPlot(chosenZipcode) {
+    getZipcode(chosenZipcode);  
+
     var trace1 = {
       x: pqi_name,
       y: y1,
-      name: 'Observed Rate ',
+      name: 'Observed Rate (per 100,000 people)',
       marker: {color: 'rgb(55, 83, 109)'},
       type: 'bar'
     };
@@ -36,106 +78,39 @@ function datas(obj,value){
     var trace2 = {
       x: pqi_name,
       y: y2,
-      name: 'Expected Rate ',
+      name: 'Expected Rate (per 100,000 people)',
       marker: {color: 'rgb(26, 118, 255)'},
       type: 'bar'
     };
 
     var data = [trace1, trace2];
-    if(value=='')
-    {
-      var text = 'Health data for all Zip code';
-    }else
-    {
-      var text = 'Health data for ' +value+' Zip code';
-    }
+
     var layout = {
+      title: 'Health data in 2015'};
 
-      title: text,
-      xaxis: {tickfont: {
-          size: 14,
-          color: 'rgb(107, 107, 107)'
-        }},
-      yaxis: {
-        title: '',
-        titlefont: {
-          size: 16,
-          color: 'rgb(107, 107, 107)'
-        },
-        tickfont: {
-          size: 14,
-          color: 'rgb(107, 107, 107)'
-        }
-      },
-      legend: {
-        x: 0,
-        y: 1.0,
-        bgcolor: 'rgba(255, 255, 255, 0)',
-        bordercolor: 'rgba(255, 255, 255, 0)'
-      },
-      barmode: 'group',
-      bargap: 0.15,
-      bargroupgap: 0.1
-    };
+    Plotly.newPlot('plotdiv', data, layout);
+};
+  
+var innerContainer = document.querySelector('[data-num="0"'),
+    plotEl = innerContainer.querySelector('.plot'),
+    zipcodeSelector = innerContainer.querySelector('.patientzipcode');
 
-    Plotly.newPlot('myDiv', data, layout);
-   /*var wk = document.getElementById('work')
-   wk.innerHTML='';
-   wk.innerHTML='Number of observartions = '+obj.length
-   wk.style.color='blue'*/
-
-}
-
-function getDropDownList(name, id, optionList) {
-    var combo = $("<select onchange='year();'></select>").attr("id", id).attr("name", name);
-    combo.append("<option value='' >Please select year</option>");
-    $.each(optionList, function (i, el) {
-        combo.append("<option value="+ el +" >" + el + "</option>");
-    });
-
-    //return combo;
-    // OR
-    $("#drop").append(combo);
-}
-
-
-
-
-
-function year()
-{
-  var getElements = function(a){
-  yourArray=[];
-  var value =   jQuery("#zipcode option:selected").val();
-  //jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
-  //.then(function(obj){
-  jsonObj=[];
-  var countPromises = xx.length;
-  var countArray = xx[0].responseJSON.length
-         for (var i = 0; i < countPromises; i++) {
-         // alert(obj[i].patient_zipcode);
-           for (var j = 0; i< countArray; i++) {
-             if(parseInt(xx[i].responseJSON[j].year)==value || value==''){
-            yourArray.push(xx);
-            item = {}
-            item ["patient_zipcode"] = xx[i].responseJSON[j].patient_zipcode;
-            item ["observed_rate_per_100_000_people"] = xx[i].responseJSON[j].observed_rate_per_100_000_people;
-            item ["year"] = xx[i].responseJSON[j].year;
-            item ["pqi_number"] = xx[i].responseJSON[j].pqi_number;
-            item ["software_version"] = xx[i].responseJSON[j].software_version;
-            item ["expected_rate_per_100_000_people"] = xx[i].responseJSON[j].expected_rate_per_100_000_people; 
-            item ["pqi_name"] = xx[i].responseJSON[j].pqi_name;
-
-             jsonObj.push(item);
-         }
-        }
-       }
-      }
-      //  getDropDownList('zipcode','zipcode', $.unique(yourArray))
-
-          datas(jsonObj,value);
-
+function assignOptions(textArray, selector) {
+  for (var i = 0; i < textArray.length;  i++) {
+      var currentOption = document.createElement('option');
+      currentOption.text = textArray[i];
+      selector.appendChild(currentOption);
   }
+}
+
+assignOptions(listofZipcode, zipcodeSelector);
+
+function updateZipcode(){
+    setBarPlot(zipcodeSelector.value);
+}
+  
+zipcodeSelector.addEventListener('change', updateZipcode, false);
+});
 
 
 var response=
@@ -186,43 +161,3 @@ jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
      })
 })
 
-
-
-
-/*
-localforage.setItem('https://health.data.ny.gov/resource/5q8c-d6xq', x).then(function () {
-  return localforage.getItem('https://health.data.ny.gov/resource/5q8c-d6xq');
-}).then(function (x) {
-  resolve(value)// we got our value
-}).catch(function (err) {
-  reject(err);// we got an error
-});
-localforage.keys('https://health.data.ny.gov/resource/5q8c-d6xq.json')
-  .then.catch(function(){
-      $.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
-      .then(function(x){
-        localforage.setItem('https://health.data.ny.gov/resource/5q8c-d6xq.json',x)
-        fun(x)
-      })
-      .fail(function(x){err(x)})
-    })
-      console.log(x)
-
-var url='https://health.data.ny.gov/resource/5q8c-d6xq.json'
-getJSON=function(url){
-    return new xx(function(resolve, reject) {
-      // do a thing, possibly async, then…
-      localforage.getItem(url)
-        .then(function(x){
-            if(x){
-                resolve(x)
-            }else{
-                $.getJSON(url)
-                 .then(function(x){
-                  localforage.setItem(url,x)
-                  resolve(x)
-                })
-                 .fail(function(err){reject(err)})
-            }})
-    })
-} */
